e0100db2dd5cf7c1b832b9aa7821bd5621d40808
Sujith-tech-code <ysvdssujith6@gmail.com>
2025-12-15 21:22:24 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,4 @@
-
 let NoofUsers=0;
-
-
 let Users=[];
 let TextAreas=[];
 // SignUP button
@@ -9,10 +6,27 @@ let SignUpButton=document.querySelector("#signupbutton");
 SignUpButton.addEventListener("click",(event)=>{
  event.preventDefault();
  NoofUsers+=1;
- Users[NoofUsers] = {
+let tog=true;
+if(tog==true){
+   Users[NoofUsers] = {
    UserName: document.querySelector("#siusername").value,
    Password: document.querySelector("#sipassword").value,
  };
+ tog=false;
+}
+else{
+  for(let i=1;i<=NoofUsers;i++){
+    if(Users[i].UserName!=document.querySelector("#siusername").value){
+         Users[NoofUsers] = {
+   UserName: document.querySelector("#siusername").value,
+   Password: document.querySelector("#sipassword").value,
+ };         break; 
+    }
+    else{
+      alert(`Username${document.querySelector("#siusername").value} already exists`);
+    }
+  }
+}
 });
 // text box area
 let TestBox=document.querySelector("#textbox");
@@ -42,4 +56,3 @@ event.preventDefault();
     }
   }
 });
-
