a8ea65a942a8a27614e03de098e605e0a4bbb9ee
Sujith-tech-code <ysvdssujith6@gmail.com>
2025-12-16 18:44:45 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,56 +1,59 @@
 let NoofUsers=0;
 let Users=[];
 let TextAreas=[];
-// SignUP button
+let PresentUserIndex;
 let SignUpButton=document.querySelector("#signupbutton");
+let LoginButton=document.querySelector("#loginbutton");
+let LUserName=document.querySelector("#lusername");
+let TestBox=document.querySelector("#textbox");
+let TextSubmitButton=document.querySelector("#textsubmit");
+let TextPath;
+
+// SignUP button
+let FirstSignUp=true;
 SignUpButton.addEventListener("click",(event)=>{
  event.preventDefault();
- NoofUsers+=1;
-let tog=true;
-if(tog==true){
+ 
+ TextPath=false;
+if(FirstSignUp==true){
+  NoofUsers+=1;
    Users[NoofUsers] = {
    UserName: document.querySelector("#siusername").value,
    Password: document.querySelector("#sipassword").value,
  };
- tog=false;
+ FirstSignUp=false;
 }
 else{
   for(let i=1;i<=NoofUsers;i++){
     if(Users[i].UserName!=document.querySelector("#siusername").value){
-         Users[NoofUsers] = {
+      NoofUsers+=1;  
+      Users[NoofUsers] = {
    UserName: document.querySelector("#siusername").value,
    Password: document.querySelector("#sipassword").value,
  };         break; 
     }
     else{
-      alert(`Username${document.querySelector("#siusername").value} already exists`);
+      alert(`Username ${document.querySelector("#siusername").value} already exists`);
     }
   }
 }
 });
+
 // text box area
-let TestBox=document.querySelector("#textbox");
-let TextSubmitButton=document.querySelector("#textsubmit");
 TextSubmitButton.addEventListener("click",(event)=>{
   event.preventDefault();
- if (NoofUsers >= 1) {
-    Users[NoofUsers].TextAreaContent = TestBox.value;
-  }
+  if(TextPath==false){Users[NoofUsers].TextAreaContent = TestBox.value;}
+    else{
+      Users[PresentUserIndex].TextAreaContent=TestBox.value;
+    }
 });
-//1st attempt to test the saved text area content
-// let testing =document.querySelector("#test");
-// testing.addEventListener("click",()=>{
-//   let testb=document.querySelector("#textbox");
-//   testb.value=Users[NoofUsers].TextAreaContent;
-// });
 
 //Login Button
-let LoginButton=document.querySelector("#loginbutton");
-let LUserName=document.querySelector("#lusername");
 LoginButton.addEventListener("click",(event)=>{
 event.preventDefault();
   for(let i=1;i<=NoofUsers;i++){
     if(LUserName.value==Users[i].UserName){
+      PresentUserIndex=i;
       TestBox.value=Users[i].TextAreaContent;
       break;
     }
